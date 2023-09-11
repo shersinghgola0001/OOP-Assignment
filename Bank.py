3816e6dec9f5a01b9e676b393d57e3ee64e631c6
SherSingh Gola <89210391+shersinghgola0001@users.noreply.github.com>
2023-09-11 19:59:59 +0530

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -31,26 +31,25 @@ Handling a Bank Account
 # Sample output
 # 100
 class Account:
-    def __init__(self, title, balance):
+    def __init__(self, title=None, balance=0):
         self.title = title
         self.balance = balance
-    def getBalance(self):
-        return self.balance
-    def deposit(self, amount):
-        self.balance += amount
     def withdrawal(self, amount):
         self.balance -= amount
+    def deposit(self, amount):
+        self.balance += amount
+    def getBalance(self):
+        return self.balance    
 class SavingsAccount(Account):
-    def __init__(self, title, balance, interestRate):
+    def __init__(self, title=None, balance=0, interestRate=0):
         super().__init__(title, balance)
         self.interestRate = interestRate
     def interestAmount(self):
         return (self.balance * self.interestRate) / 100
-account = Account("Ashish", 2000)
-savings_account = SavingsAccount("Ashish", 2000, 5)
-account.deposit(500)
-print("Account Balance:", account.getBalance()) 
-account.withdrawal(500)
-print("Account Balance:", account.getBalance()) 
-interest = savings_account.interestAmount()
-print("Savings Account Interest Amount:", interest) 
+demo1 = SavingsAccount("Ashish", 2000, 5)   
+demo1.deposit(500)  
+print("Current Balance:", demo1.getBalance())  
+demo1.withdrawal(500) 
+print("Current Balance:", demo1.getBalance())  
+interest = demo1.interestAmount()  
+print("Interest Amount:", interest)
